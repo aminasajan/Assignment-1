24fd6e915749238502768759433d58640f7d4903
Amina Sajan <aminasajan2001@gmail.com>
2023-05-22 13:14:34 +0530

    First Commit

---
+++ gitassignment1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("enter first no."))
+b=int(input("enter second no."))
+t=a
+a=b
+b=a
+print("first no.= ",a)
+print("second no.= ",b)
+
+
+# In[ ]:
+
+
+
+
